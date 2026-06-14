f193a445e44b96fb9c5138479af5aa4613a15ad2
roshan-neupaney <roshanneupane600@gmail.com>
2026-06-14 18:12:31 +0545

    fix:: handle error message for same email id

--- authentication/views.py
+++ authentication/views.py
@@ -10,11 +10,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class RegistrationView(APIView):
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(
-                {"message": "Account Created Successfully"},
-                status=status.HTTP_201_CREATED,
+                {"message": "Account Created Successfully", "status": status.HTTP_201_CREATED},
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
