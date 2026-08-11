02ef2ce3fb4091ac43e13c5c1f57f268c49b060c
roshan-neupaney <roshanneupane600@gmail.com>
2026-08-11 08:54:33 +0545

    feat:: add user, payments, subscriptions api

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+'COMPLETE | PENDING | FAILURE | FULL_REFUND | PARTIAL_REFUND | AMBIGUOUS | NOT_FOUND | CANCELED'
+StatusChoices = [
+    ("COMPLETE", "COMPLETE"),
+    ("PENDING", "PENDING"),
+    ("FAILURE", "FAILURE"),
+    ("FULL_REFUND", "FULL_REFUND"),
+    ("PARTIAL_REFUND", "PARTIAL_REFUND"),
+    ("AMBIGUOUS", "AMBIGUOUS"),
+    ("NOT_FOUND", "NOT_FOUND"),
+    ("CANCELED", "CANCELED"),
+]
+
+class PaymentMethods(models.Model):
+    title = models.CharField(max_length=100)
+    logo = models.CharField(max_length=300, blank=True, null=True)
+    config = models.JSONField(default=dict, blank=True)
+    gateway_type = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class PaymentHistorys(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payment_history')
+    subscription = models.ForeignKey('subscriptions.Subscriptions', on_delete=models.SET_NULL, null=True, related_name='payment_history')
+    payment_method = models.ForeignKey(PaymentMethods, on_delete=models.SET_NULL, null=True, related_name='payment_history')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_uuid = models.CharField(max_length=100, blank=True, unique=True, null=True)
+    gateway_response = models.JSONField(default=dict, blank=True)
+    status = models.CharField(max_length=50, default='PENDING', choices=StatusChoices)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
