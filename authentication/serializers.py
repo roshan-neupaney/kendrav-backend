6afe45e792f403058c26ea6e74ef9e464bee074f
Roshan Sageflick <roshanneupane600@gmail.com>
2026-08-12 18:24:40 +0545

    update api urls

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers, status
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from users.models import UserToken
 
