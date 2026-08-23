6109753a5a43623114033d12a662015e860526c2
roshan-neupaney <roshanneupane600@gmail.com>
2026-08-23 19:15:51 +0545

    feat:: complete user api

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Profile, Preference, UserSubscription
-from notifications.models import UserNotification
+from workspaces.models import Workspace
+from notifications.models import NotificationPreference
 
 User = get_user_model()
 
@@ -9,24 +10,66 @@ User = get_user_model()
 class PreferencesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Preference
-        fields = "__all__"
         exclude = ["user"]
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = "__all__"
+        exclude = ["user"]
+
+
+class UserSubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSubscription
+        exclude = ["user"]
+
+class WorkspaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        exclude = ["owner"]
+
+
+class NotificationPreferenceSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationPreference
         exclude = ["user"]
 
 
 class UserSerializers(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    preference = PreferencesSerializer(read_only=True)
+    workspaces = WorkspaceSerializer(many=True, read_only=True)
+    notification_preferences = NotificationPreferenceSerializers(
+        many=True, read_only=True
+    )
+
+    user_subscription = serializers.SerializerMethodField()
+    def get_user_subscription(self, instance):
+        subscription = UserSubscription.objects.filter(user=instance,is_active=True).first()
+        if subscription:
+            return UserSubscriptionSerializer(subscription).data
+        return None
+
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "profile",
+            "preference",
+            "workspaces",
+            "user_subscription",
+            "notification_preferences",
+        ]
 
-class UserSubsctiptions(serializers.ModelSerializer):
-    class Meta:
-        model = UserSubscription
-        fields = '__all__'
-        exclude = ['user']
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        notification_preferences = data.pop("notification_preferences", [])
+        result = {}
+        for item in notification_preferences:
+            result[item["notification_type"]] = item["is_permitted"]
+
+        data["notification_preferences"] = result
+        return data
--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from .serializers import UserSerializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserView(APIView):
     def get(self, request):
         user = request.user
         serializer = UserSerializers(user)
-        return Response(serializer.data)
+        return Response(
+            {
+                "message": "Users fetched successfully.",
+                "data": serializer.data,
+                "status": status.HTTP_200_OK,
+            },
+            status=status.HTTP_200_OK,
+        )
